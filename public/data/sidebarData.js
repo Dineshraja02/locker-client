ecd09d1e86beb382439aa9d30d9c12736d12f4df
Dinesh Raja <dineshraja0403@gmail.com>
2022-01-05 12:30:24 +0530

    new project

---
+++ public/data/sidebarData.js
@@ -0,0 +1,27 @@
+export const SidebarData = [
+    {
+        title:"Dashboard",
+        icon:"",
+        link:""
+    },
+    {
+        title:"Reports",
+        icon:"",
+        link:""
+    },
+    {
+        title:"User Profile",
+        icon:"",
+        link:""
+    },
+    {
+        title:"Emp Profile",
+        icon:"",
+        link:""
+    },
+    {
+        title:"Super Admin Ctrl",
+        icon:"",
+        link:""
+    },
+]
